9cdaf10e0c31f35237ca06d14d63b2b736032dfe
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2025-02-23 17:10:44 +0900

    docs: octal.ts にコメント追加

--- packages/core/libs/octal.ts
+++ packages/core/libs/octal.ts
@@ -1,28 +1,47 @@
+/**
+ * 引数で受け取った文字列が 8 進数 (正規表現: `[0-7]`) かどうかを判定する。
+ *
+ * @param char 長さ1の文字列
+ * @returns 0 から 7 の文字のときは `true`、それ以外は `false`
+ */
 export const isOctalDigit = (char: string): boolean => {
   return "0" <= char && char <= "7";
 };
 
+/**
+ * 引数で受け取った文字列が ASCII 文字を表現する 8 進数 (`[0-7][0-7]?|[0-2][0-7][0-7]`) かどうかを判定する。
+ *
+ * @param str 文字列
+ * @returns ASCII 文字を表現する 8 進数のときは `true`、それ以外は `false`
+ */
 export const isOctalAscii = (str: string): boolean => {
   if (str.length === 1) {
     return isOctalDigit(str);
   }
 
   if (str.length === 2) {
-    return isOctalAscii(str.charAt(0)) && isOctalAscii(str.charAt(1));
+    return isOctalDigit(str.charAt(0)) && isOctalDigit(str.charAt(1));
   }
 
   if (str.length === 3) {
+    const char = str.charAt(0);
     return (
-      "0" <= str.charAt(0) &&
-      str.charAt(0) <= "2" &&
-      isOctalAscii(str.charAt(1)) &&
-      isOctalAscii(str.charAt(2))
+      "0" <= char &&
+      char <= "2" &&
+      isOctalDigit(str.charAt(1)) &&
+      isOctalDigit(str.charAt(2))
     );
   }
 
   return false;
 };
 
+/**
+ * ASCII 文字を表現する 8 進数を文字に変換して返す。
+ *
+ * @param char ASCII 文字を表現する 8 進数
+ * @returns ASCII 文字
+ */
 export const octalDigitToChar = (char: string): string => {
   return String.fromCharCode(Number.parseInt(char, 8));
 };
