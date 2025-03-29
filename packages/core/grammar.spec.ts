acbe987c5f427dd2e079b4ff4741b2a17a03172e
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2025-03-29 23:03:14 +0900

    feat: use tpeg

--- packages/core/grammar.spec.ts
+++ packages/core/grammar.spec.ts
@@ -1,39 +1,11 @@
 import { describe, expect, it, jest } from "bun:test";
-import {
-  andPredicate,
-  char,
-  characterClass,
-  close,
-  comment,
-  definition,
-  dot,
-  endOfFile,
-  endOfLine,
-  expression,
-  grammar,
-  identifier,
-  leftArrow,
-  literal,
-  notPredicate,
-  open,
-  plus,
-  prefix,
-  primary,
-  question,
-  range,
-  semicolon,
-  sequence,
-  slash,
-  space,
-  star,
-  suffix,
-} from "./grammar";
+import { grammar } from "./grammar";
 
 describe("", () => {
   it("", async () => {
     const pegFile = await Bun.file("./samples/peg.peg").text();
 
-    const result = grammar(pegFile, 0);
+    const result = grammar(pegFile, { offset: 0, column: 0, line: 1 });
 
     console.log(JSON.stringify(result, null, 2));
   });
